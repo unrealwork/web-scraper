ad24e38748daa83ebf1ce9ec652d04edb17060b7
unrealwork <unrealwork@gmail.com>
2017-11-10 06:16:06 +0300

    test and checkstyle violations

--- src/main/java/com/unrealwork/webscraper/WebScraper.java
+++ src/main/java/com/unrealwork/webscraper/WebScraper.java
@@ -2,9 +2,23 @@ package com.unrealwork.webscraper;
 
 import java.util.logging.Logger;
 
-public class WebScraper {
+/**
+ * Main executable class.
+ */
+public final class WebScraper {
 
-  public static void main(String[] args) {
+  /**
+   * Hide constructor for safety.
+   */
+  private WebScraper() {
+  }
+
+  /**
+   * Main entry point to program.
+   *
+   * @param args program arguments described in readme file.
+   */
+  public static void main(final String[] args) {
     Logger log = Logger.getLogger(WebScraper.class.getName());
     log.info("Hello I'm Web Scraper");
   }
